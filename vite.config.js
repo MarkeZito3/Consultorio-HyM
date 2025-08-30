d674991d3cfd69a6f0adb967ea15b0afa8d42320
Marcos <markkesgames@hotmail.com>
2025-08-29 21:45:21 -0400

    vite-plugin-static-copy incorporated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/assets/services', dest: 'assets' },
+        { src: 'src/assets/jurisprudencias', dest: 'assets' },
+        { src: 'src/assets/team', dest: 'assets' },
+      ],
+    }),
+  ],
   server: {
     port: 3000,
     open: true
